b0700752e79f476d9e527a5773915001d441d297
kerembilim <kerembilim@gmail.com>
2020-03-22 20:34:39 +0300

    [ADD] Added jwt module.

--- chatAppAuthenticationService/routes/users.js
+++ chatAppAuthenticationService/routes/users.js
@@ -1,24 +1,50 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
-
+const tokenControl = (req,res,next) =>{
+  const bearerHeader = req.headers["authorization"];
+  if(typeof bearerHeader != 'undefined'){
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    jwt.verify(bearerToken,'my_secret_key',(err,data)=>{
+      if(err){
+        res.sendStatus(403);
+      }
+      else
+        req.userData = data;
+        next();
+    })
+     
+  }
+  else{
+    res.sendStatus(403);
+  }
+};
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-/*router.get('/all',  (req, res, next) =>{
-  connection.query('SELECT * FROM User', (err, result) => {
-    res.json({result});
+
+router.post('/login',  (req, res, next) =>{// db işlemleri
+  const user = req.body;
+  const token = jwt.sign( { user }, 'my_secret_key');//secret verilecek
+  res.json({
+    token
+  });
+});
+
+router.get('/protected', tokenControl,(req, res, next) =>{
+  var ees = req.userData;
+  res.json({
+    status:'this is protected',
+    ees
   });
 });
 
 
-router.get('/sql/:id',  (req, res, next) =>{
-  connection.query('SELECT * FROM User where id= '+req.params.id, (err, result) => {
-    res.json({result});
-  });
-});*/
 
 
 module.exports = router;
