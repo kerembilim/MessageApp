f1a11323bd544a84459be933f89fb8af2a25c3f0
kerembilim <kerembilim@gmail.com>
2020-03-30 00:28:20 +0300

    [ADD] Created db connection model.

--- chatAppAuthenticationService/config.js
+++ chatAppAuthenticationService/config.js
@@ -1,4 +1,16 @@
 const dbPassword = 'messageappdbsecretpassword';
+const dbUser = 'postgres';
+const dbHost = 'localhost';
+const dbName = 'AUTHENTICATION_DB';
+const dbPort = '5432';
+const jwtSecretKey = 'GHbW35sYAfY86tXyqhMnyx7bHLPt3Zdz';
+const cryptionKey = 'Y4FZkFeFNxcBLtdPEK6rBxwqaj8Ffp86';
 
 exports.dbPassword = dbPassword;
+exports.dbUser = dbUser;
+exports.dbHost = dbHost;
+exports.dbName = dbName;
+exports.dbPort = dbPort;
+exports.jwtSecretKey = jwtSecretKey;
+exports.cryptionKey = cryptionKey;
 
--- chatAppAuthenticationService/routes/users.js
+++ chatAppAuthenticationService/routes/users.js
@@ -1,6 +1,29 @@
 var express = require('express');
 var router = express.Router();
 var jwt = require('jsonwebtoken');
+var CryptoJS = require("crypto-js");
+const config = require('../config');
+
+const { Pool } = require('pg')
+
+let pool;
+
+const connectionInitialize = () =>{
+ pool = new Pool({
+  user: config.dbUser,
+  host: config.dbHost,
+  database: config.dbName,
+  password: config.dbPassword,
+  port: config.dbPort,
+  });
+}
+
+const connectionDisposeAndDisconnect = () => {
+  pool.end();
+}
+
+
+
 
 const tokenControl = (req,res,next) =>{
   const bearerHeader = req.headers["authorization"];
@@ -8,13 +31,35 @@ const tokenControl = (req,res,next) =>{
     const bearer = bearerHeader.split(" ");
     const bearerToken = bearer[1];
     req.token = bearerToken;
-    jwt.verify(bearerToken,'my_secret_key',(err,data)=>{
+    jwt.verify(bearerToken,config.jwtSecretKey,(err,data)=>{
       if(err){
-        res.sendStatus(403);
+        res.sendStatus(403).send({ error: 'Invalid Token.' });
       }
-      else
-        req.userData = data;
-        next();
+      else{
+        const resDb =  pool.query('SELECT * FROM users WHERE username =' + req.body.username);
+        pool.end();
+        if( resDb != null && resDb.rows[0] != null ){
+          var pwBytes  = CryptoJS.AES.decrypt(resDb.rows[0].password, config.cryptionKey );
+          var password = pwBytes.toString(CryptoJS.enc.Utf8);
+          if(password === resDb.rows[0].password){
+            const responseData = {
+              userId : resDb.rows[0].id,
+              username : resDb.rows[0].username,
+              token : data
+            }
+            req.userData = resDb.rows[0];
+            next();
+          }
+          else{
+            res.sendStatus(403).send({ error: 'Username or password is wrong.' });
+          }
+          
+        }
+        else{
+
+        }
+      }
+        
     })
      
   }
@@ -23,17 +68,39 @@ const tokenControl = (req,res,next) =>{
   }
 };
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async(req, res, next) => {
+  const resDb = await pool.query('SELECT NOW()');
+  await pool.end();
+  res.json(resDb.rows[0])
 });
 
 
-router.post('/login',  (req, res, next) =>{// db işlemleri
+router.post('/login',  (req, res, next) =>{
   const user = req.body;
-  const token = jwt.sign( { user }, 'my_secret_key');//secret verilecek
+  const token = jwt.sign( { user }, config.jwtSecretKey);
   res.json({
     token
   });
+});
+
+router.post('/register',async(req, res, next) =>{
+  const user = req.body;
+  connectionInitialize();
+  let sqlText = "SELECT count(*) FROM users WHERE username = '" + user.username + "'";
+  console.log(sqlText);
+  const userCount = await pool.query(sqlText);
+  console.log( userCount.rows[0]);
+  if(userCount != null && userCount.rows[0].count === '0'){
+    sqlText = "INSERT INTO USERS (username,password) values('"+user.username+"','"+CryptoJS.AES.encrypt(user.password, config.cryptionKey).toString()+"')";
+    const resDb = await pool.query(sqlText);
+    connectionDisposeAndDisconnect();
+    res.json({Statu:'User Saved'});
+  }
+  else{
+    res.json({
+      Error:'User already exist'
+    })
+  }
 });
 
 router.get('/protected', tokenControl,(req, res, next) =>{
