af60f0206f79216d800e2ee81bb372c8392be7ce
kerembilim <kerembilim@gmail.com>
2020-05-01 17:40:57 +0300

    [FIX] Fixed get contact problem

--- chatAppUi/src/App.js
+++ chatAppUi/src/App.js
@@ -2,8 +2,11 @@ import React from 'react';
 import './App.css';
 import ReactModal from 'react-modal';
 import messageBackground from './utilies/messageBackground.jpeg';
-import DownloadFile from './DownloadFile';
+import DownloadFile from './components/DownloadFile';
 import axios, { post } from 'axios';
+
+//pages
+import MessagePage from './pages/MessagePage'
 
 const io = require('socket.io-client');
 
@@ -15,115 +18,44 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { value: '', password: '', showModal: true, username: '', contacts: [], messageTarget: {}, messages: [], file: null };
-    this.handleChange = this.handleChange.bind(this);
     this.handleChangeUsername = this.handleChangeUsername.bind(this);
     this.handleChangePass = this.handleChangePass.bind(this);
-    this.sendMessage = this.sendMessage.bind(this);
     this.handleCloseModal = this.handleCloseModal.bind(this);
     this.loginControl = this.loginControl.bind(this);
-    this.getContacts = this.getContacts.bind(this);
-    this.getMessages = this.getMessages.bind(this);
-    this.addMessageArea = this.addMessageArea.bind(this);
     this.login = this.login.bind(this);
-    this.onFormSubmit = this.onFormSubmit.bind(this)
-    this.onChange = this.onChange.bind(this)
-    this.fileUpload = this.fileUpload.bind(this)
   }
 
   async componentDidMount() {
     await this.loginControl();
-    await this.getMessages();
-
-  }
-  onFormSubmit(e) {
-    e.preventDefault() // Stop form submit
-    this.fileUpload(this.state.file).then((response) => {
-      console.log(response.data);
-    })
-  }
-  onChange(e) {
-    this.setState({ file: e.target.files[0] })
-    this.fileUpload(this.state.file);
-  }
-  fileUpload(file) {
-    const url = 'http://localhost:4010/fileupload/messagefileupload';
-    const formData = new FormData();
-    formData.append('file', file)
-    const config = {
-      headers: {
-        'content-type': 'multipart/form-data',
-        'Authorization': 'Bearer ' + localStorage.getItem('userToken')
-      }
-    }
-    return post(url, formData, config)
   }
 
-  enterClick = (e) => {
-    if (e.keyCode === 13 && this.state.value != '') {
-      const message = {
-        content: this.state.value,
-        target: this.state.messageTarget.username,
-        sender: this.state.username
-      }
-      this.sendMessage(message);
-      this.setState({ value: '' });
-      document.getElementById('messageArea').innerHTML = "";
-    }
-
+  handleChangeUsername(event) {
+    this.setState({ username: event.target.value });
   }
 
-  addMessageArea(message) {
-    var css, head, style, node;
-    if (message.sender === this.state.username) {
-      css = 'mymessage { background: #baecba; float:right; clear: both;padding:2%; margin:1%;max-width:300px; word-wrap:break-word;border-radius:5px; }';
-      head = document.head || document.getElementsByTagName('head')[0];
-      style = document.createElement('style');
-      node = document.createElement("mymessage");
-    }
-    else {
-      css = 'stranger { background: #fffdfa; float:left; clear: both;padding:2%; margin:1%;max-width:300px; word-wrap:break-word;border-radius:5px; }';
-      head = document.head || document.getElementsByTagName('head')[0];
-      style = document.createElement('style');
-      node = document.createElement("stranger");
-    }
-    head.appendChild(style);
-
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      // This is required for IE8 and below.
-      style.styleSheet.cssText = css;
-    } else {
-      style.appendChild(document.createTextNode(css));
-    }
-
-    var textnode = document.createTextNode(message.sender + ': ' + message.content);
-    node.appendChild(textnode);
-    document.getElementById("messageArea").appendChild(node);
-
-
+  handleChangePass(event) {
+    this.setState({ password: event.target.value });
   }
 
-
-  sendMessage(message) {
-    this.socket.emit('chatmessage', message);
-    let messages = this.state.messages;
-    messages.push(message);
-    this.setState({ messages });
-  }
 
   loginControl = async () => {
-    var self = this;
-    await axios.post('http://localhost:5000/users/loginControl', {
-    }, {
-        headers: { Authorization: "Bearer " + localStorage.getItem('userToken') }
-      }).then(async function (response) {
-        if (response.data.result === 'basarili') {
-          await self.setState({ showModal: false });
-          await self.setState({ username: response.data.username });
-          await self.initSocketConnection();
-          await self.getContacts();
-        }
-      });
+    if (localStorage.getItem('userToken') == null || localStorage.getItem('userToken') == '') {
+      this.login()
+    }
+    else {
+      console.log(localStorage.getItem('userToken'))
+      var self = this;
+      await axios.post('http://localhost:5000/users/loginControl', {
+      }, {
+          headers: { Authorization: "Bearer " + localStorage.getItem('userToken') }
+        }).then(async function (response) {
+          if (response.data.result === 'basarili') {
+            await self.setState({ showModal: false });
+            await self.setState({ username: response.data.username });
+          }
+        });
+    }
+
   }
 
   handleCloseModal() {
@@ -139,62 +71,14 @@ class App extends React.Component {
       if (response.data.result === 'basarili') {
         self.setState({ showModal: false });
         localStorage.setItem("userToken", response.data.token);
+        localStorage.setItem("userName", response.data.username);
         self.setState({ username: response.data.username });
-        self.initSocketConnection();
-        self.getContacts = self.getContacts.bind(self);
-        self.getContacts();
+         window.location.reload()
       }
-
-    })
-  }
-
-  getContacts() {
-    var self = this;
-    axios.get('http://localhost:5000/users/getContacts' + '/' + this.state.username, {
-      headers: { Authorization: "Bearer " + localStorage.getItem('userToken') }
-    }).then(function (response) {
-      self.setState({ contacts: response.data });
-      self.list = response.data;
-    })
-  }
-
-  getMessages = async () => {
-    var self = this;
-    await axios.get('http://localhost:4000/getmessages', {
-      params: {
-        username: this.state.username
-      }
-    }).then(async function (response) {
-      await self.setState({ messages: response.data });
     })
   }
 
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-    document.getElementById('messageArea').innerHTML = "";
-
-  }
-
-  handleChangeUsername(event) {
-    this.setState({ username: event.target.value });
-  }
-
-  handleChangePass(event) {
-    this.setState({ password: event.target.value });
-  }
-
-  initSocketConnection() {
-    var self = this;
-
-    this.socket = io.connect('http://localhost:4000');
-    this.socket.on(this.state.username, async (msg) => {
-      let newMessages = self.state.messages;
-      newMessages.push(msg);
-      document.getElementById('messageArea').innerHTML = "";
-      self.setState({ messages: newMessages });
-    });
-  }
   myFunction() {
     var x = document.getElementById("mySelect").value;
     console.log(x);
@@ -202,10 +86,10 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="bg-dim full-bg-size" style={{ backgroundImage: `url(${messageBackground})` }}>
+      <div >
         <header className="header">
           <div className="wrap">
-            <h2 className="logo"><a href="#">Website Logo</a></h2>
+            <h2 className="logo"><a href="">Website Logo</a></h2>
             <select id="mySelect" onChange={this.myFunction}>
               <option value="volvo">Volvo</option>
               <option value="saab">Saab</option>
@@ -221,6 +105,7 @@ class App extends React.Component {
 
           </div>
         </header>
+        
         <ReactModal
           ariaHideApp={false}
           isOpen={this.state.showModal}
@@ -234,7 +119,6 @@ class App extends React.Component {
               margin: 'auto',
               paddingTop: 0
             }
-
           }}
         >
           <p style={{ fontSize: 20 }}>Login</p>
@@ -247,60 +131,11 @@ class App extends React.Component {
           <input type="submit" style={{ float: 'right' }} value="GÖNDER" onClick={this.login} />
         </ReactModal>
 
-        <div className="connectList" >
-          {this.state.contacts.map(index =>
-            <div key={index.username} className="connect" onClick={() => { this.setState({ messageTarget: index }); document.getElementById('messageArea').innerHTML = ""; }} >
-              <img src={index.image} style={{ height: 70, width: 70, float: 'left', borderRadius: 50, marginTop: 10, marginBottom: 10, backgroundSize: 'contain' }} />
-              <div>
-                <p style={{ marginLeft: 120, fontSize: 30, marginTop: 5, marginBottom: 0 }} >{index.username}</p>
-                <p style={{ marginLeft: 120, fontSize: 15, marginTop: 20, color: 'green' }} >{index.statu}</p>
-              </div>
-
-            </div>)}
+        <div className="bg-dim full-bg-size" style={{ backgroundImage: `url(${messageBackground})` }}>
+          <MessagePage
+            username={this.state.username} />
         </div>
-        <div className="allMessageArea" id='' >
-          {
-            this.state.messageTarget.username ?
-              <div className="messageTarget">
-                <img src={this.state.messageTarget.image} style={{ height: 50, width: 50, float: 'left', borderRadius: 50, marginTop: 10, marginLeft: 10, marginBottom: 10, backgroundSize: 'contain' }} />
-                <div style={{ paddingTop: 10, marginLeft: 100, fontSize: 30 }}>{this.state.messageTarget.username}</div>
-                <div style={{ paddingTop: 10, color: 'green', marginLeft: 110, fontSize: 15 }}>{this.state.messageTarget.statu}</div>
-              </div> : null
-          }
-          <div id='messageArea' >
-            {
-              this.state.messages.map(index => {
-                if (index.target == this.state.messageTarget.username || index.sender == this.state.messageTarget.username) {
-                  this.addMessageArea(index);
-                }
-              })
-            }
-          </div>
-        </div>
-        {
-          this.state.messageTarget.username ?
-            <div>
-              <input type="text" onKeyUp={this.enterClick} id='messageText' className="message" value={this.state.value} onChange={this.handleChange} />
-              <input id="btnSend" type="submit" className="button" value="GÖNDER" onClick={() => {
-                if (this.state.value != '') {
-                  const message = {
-                    content: this.state.value,
-                    target: this.state.messageTarget.username,
-                    sender: this.state.username
-                  }
-                  this.sendMessage(message);
-                  this.setState({ value: '' });
-                  document.getElementById('messageArea').innerHTML = "";
-                }
 
-              }} />
-
-
-              <input type="file" onChange={this.onChange} className="button" />
-              <button type="submit" className="button" onClick={this.onFormSubmit}>Upload</button>
-
-            </div> : null
-        }
 
       </div>
 
---
+++ chatAppUi/src/pages/MessagePage.js
@@ -0,0 +1,231 @@
+import React, { Component } from 'react';
+import '../App.css';
+import axios, { post } from 'axios';
+
+const io = require('socket.io-client');
+
+export default class MessagePage extends Component {
+
+  socket = null;
+  list = [];
+
+
+
+  constructor(props) {
+    super(props);
+    this.state = { value: '', password: '', showModal: true, username: '', contacts: [], messageTarget: {}, messages: [], file: null };
+    this.handleChange = this.handleChange.bind(this);
+
+    this.sendMessage = this.sendMessage.bind(this);
+    this.getContacts = this.getContacts.bind(this);
+    this.getMessages = this.getMessages.bind(this);
+    this.addMessageArea = this.addMessageArea.bind(this);
+    this.loginControl = this.loginControl.bind(this);
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+    this.onChange = this.onChange.bind(this)
+    this.fileUpload = this.fileUpload.bind(this)
+  }
+
+  async componentDidMount() {
+    //this.state.username = localStorage.get('');
+    await this.loginControl();
+    await this.getMessages();
+    await this.initSocketConnection();
+    await this.getContacts();
+  }
+
+  loginControl = async () => {
+
+    if (localStorage.getItem('userToken') !== null && localStorage.getItem('userToken') !== '') {
+      console.log(localStorage.getItem('userToken'));
+      var self = this;
+      await axios.post('http://localhost:5000/users/loginControl', {}, {
+        headers: { Authorization: "Bearer " + localStorage.getItem('userToken') }
+      }).then(async function (response) {
+        if (response.data.result === 'basarili') {
+          await self.setState({ username: response.data.username });
+        }
+      });
+    }
+
+  }
+
+
+  getContacts() {
+    if (localStorage.getItem('userToken') !== null && localStorage.getItem('userToken') !== '') {
+      var self = this;
+      axios.get('http://localhost:5000/users/getContacts' + '/' + this.state.username, {
+        headers: { Authorization: "Bearer " + localStorage.getItem('userToken') }
+      }).then(function (response) {
+        self.setState({ contacts: response.data });
+        self.list = response.data;
+      })
+    }
+  }
+
+  getMessages = async () => {
+    var self = this;
+    await axios.get('http://localhost:4000/getmessages', {
+      params: {
+        username: this.state.username
+      }
+    }).then(async function (response) {
+      await self.setState({ messages: response.data });
+    })
+  }
+
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+    document.getElementById('messageArea').innerHTML = "";
+
+  }
+
+
+  initSocketConnection() {
+    var self = this;
+
+    this.socket = io.connect('http://localhost:4000');
+    this.socket.on(this.state.username, async (msg) => {
+      let newMessages = self.state.messages;
+      newMessages.push(msg);
+      document.getElementById('messageArea').innerHTML = "";
+      self.setState({ messages: newMessages });
+    });
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault() // Stop form submit
+    this.fileUpload(this.state.file).then((response) => {
+      console.log(response.data);
+    })
+  }
+  onChange(e) {
+    this.setState({ file: e.target.files[0] })
+    this.fileUpload(this.state.file);
+  }
+  fileUpload(file) {
+    const url = 'http://localhost:4010/fileupload/messagefileupload';
+    const formData = new FormData();
+    formData.append('file', file)
+    const config = {
+      headers: {
+        'content-type': 'multipart/form-data',
+        'Authorization': 'Bearer ' + localStorage.getItem('userToken')
+      }
+    }
+    return post(url, formData, config)
+  }
+
+  enterClick = (e) => {
+    if (e.keyCode === 13 && this.state.value !== '') {
+      const message = {
+        content: this.state.value,
+        target: this.state.messageTarget.username,
+        sender: this.state.username
+      }
+      this.sendMessage(message);
+      this.setState({ value: '' });
+      document.getElementById('messageArea').innerHTML = "";
+    }
+
+  }
+
+  addMessageArea(message) {
+    var css, head, style, node;
+    if (message.sender === this.state.username) {
+      css = 'mymessage { background: #baecba; float:right; clear: both;padding:2%; margin:1%;max-width:300px; word-wrap:break-word;border-radius:5px; }';
+      head = document.head || document.getElementsByTagName('head')[0];
+      style = document.createElement('style');
+      node = document.createElement("mymessage");
+    }
+    else {
+      css = 'stranger { background: #fffdfa; float:left; clear: both;padding:2%; margin:1%;max-width:300px; word-wrap:break-word;border-radius:5px; }';
+      head = document.head || document.getElementsByTagName('head')[0];
+      style = document.createElement('style');
+      node = document.createElement("stranger");
+    }
+    head.appendChild(style);
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+      // This is required for IE8 and below.
+      style.styleSheet.cssText = css;
+    } else {
+      style.appendChild(document.createTextNode(css));
+    }
+
+    var textnode = document.createTextNode(message.sender + ': ' + message.content);
+    node.appendChild(textnode);
+    document.getElementById("messageArea").appendChild(node);
+
+
+  }
+
+
+  sendMessage(message) {
+    this.socket.emit('chatmessage', message);
+    let messages = this.state.messages;
+    messages.push(message);
+    this.setState({ messages });
+  }
+  render() {
+    return (
+      <div className="bg-dim" >
+        <div className="connectList" >
+          {this.state.contacts.map(index =>
+            <div key={index.username} className="connect" onClick={() => { this.setState({ messageTarget: index }); document.getElementById('messageArea').innerHTML = ""; }} >
+              <img alt={index.username} src={index.image} style={{ height: 70, width: 70, float: 'left', borderRadius: 50, marginTop: 10, marginBottom: 10, backgroundSize: 'contain' }} />
+              <div>
+                <p style={{ marginLeft: 120, fontSize: 30, marginTop: 5, marginBottom: 0 }} >{index.username}</p>
+                <p style={{ marginLeft: 120, fontSize: 15, marginTop: 20, color: 'green' }} >{index.statu}</p>
+              </div>
+
+            </div>)}
+        </div>
+        <div className="allMessageArea" id='' >
+          {
+            this.state.messageTarget.username ?
+              <div className="messageTarget">
+                <img src={this.state.messageTarget.image} style={{ height: 50, width: 50, float: 'left', borderRadius: 50, marginTop: 10, marginLeft: 10, marginBottom: 10, backgroundSize: 'contain' }} />
+                <div style={{ paddingTop: 10, marginLeft: 100, fontSize: 30 }}>{this.state.messageTarget.username}</div>
+                <div style={{ paddingTop: 10, color: 'green', marginLeft: 110, fontSize: 15 }}>{this.state.messageTarget.statu}</div>
+              </div> : null
+          }
+          <div id='messageArea' >
+            {
+              this.state.messages.map(index => {
+                if (index.target === this.state.messageTarget.username || index.sender === this.state.messageTarget.username) {
+                  this.addMessageArea(index);
+                }
+              })
+            }
+          </div>
+        </div>
+        {
+          this.state.messageTarget.username ?
+            <div>
+              <input type="text" onKeyUp={this.enterClick} id='messageText' className="message" value={this.state.value} onChange={this.handleChange} />
+              <input id="btnSend" type="submit" className="button" value="GÖNDER" onClick={() => {
+                if (this.state.value !== '') {
+                  const message = {
+                    content: this.state.value,
+                    target: this.state.messageTarget.username,
+                    sender: this.state.username
+                  }
+                  this.sendMessage(message);
+                  this.setState({ value: '' });
+                  document.getElementById('messageArea').innerHTML = "";
+                }
+
+              }} />
+
+
+              <input type="file" onChange={this.onChange} className="button" />
+              <button type="submit" className="button" onClick={this.onFormSubmit}>Upload</button>
+
+            </div> : null
+        } </div>
+    );
+  }
+}
